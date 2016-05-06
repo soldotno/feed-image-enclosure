4b033d2ecf8ecdf3195b6d67327d687c36229b91
Just Thomas Misund <thomas@hemmeligadresse.com>
2016-05-06 21:10:59 +0200

    Use slightly different technique to get image url

--- feed-image-enclosure.php
+++ feed-image-enclosure.php
@@ -11,23 +11,28 @@ License: GPL2
 
 add_action( 'rss2_item', 'add_post_featured_image_as_rss_item_enclosure' );
 
-function add_post_featured_image_as_rss_item_enclosure() {
-	if ( ! has_post_thumbnail() )
+function add_post_featured_image_as_rss_item_enclosure( $post_id=0 ) {
+	global $post;
+	$post_id = $post_id ? $post_id : $post->ID;
+
+	if ( ! has_post_thumbnail( $post_id ) )
 		return;
 
-	$thumbnail_size = apply_filters( 'rss_enclosure_image_size', 'thumbnail' );
-	$thumbnail_id = get_post_thumbnail_id( get_the_ID() );
-	$thumbnail = image_get_intermediate_size( $thumbnail_id, $thumbnail_size );
-
-	if ( empty( $thumbnail ) )
-		return;
-
-	$upload_dir = wp_upload_dir();
+	$thumb_id = get_post_thumbnail_id( $post_id );
+	
+	// Get image meta data and create an object out of it (WordPress returns just an array);
+	$thumb_srcdata = wp_get_attachment_image_src( $thumb_id, 'thumbnail-size', false );
+	$image = array(
+		'src' => $thumb_srcdata[0],
+		'width' => $thumb_srcdata[1],
+		'height' => $thumb_srcdata[2],
+		'resized' => $thumb_srcdata[3],
+	);
 
 	printf( 
 		'<enclosure url="%s" length="%s" type="%s" />',
-		$thumbnail['url'], 
+		$image['src'], 
 		"", // filesize( path_join( $upload_dir['basedir'], $thumbnail['path'] ) ), 
-		get_post_mime_type( $thumbnail_id ) 
+		get_post_mime_type( $thumb_id ) 
 	);
 }
